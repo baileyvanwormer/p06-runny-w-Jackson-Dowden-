922b7acb56acf68bd349054bc371c9b0bc18b072
Bailey Van Wormer <bvan-wormer@unc.edu>
2022-03-08 17:20:21 -0500

    Initial Commit

---
+++ p06-runny/p06_runnyApp.swift
@@ -0,0 +1,17 @@
+//
+//  p06_runnyApp.swift
+//  p06-runny
+//
+//  Created by Bailey Van Wormer on 3/8/22.
+//
+
+import SwiftUI
+
+@main
+struct p06_runnyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
